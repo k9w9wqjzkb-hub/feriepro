30e45343694fd84a4606bf6b8396fc277965f573
Sergio <k9w9wqjzkb@privaterelay.appleid.com>
2026-02-18 12:30:00 +0100

    Delete sw.v23.js

--- sw.v23.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME = 'iwork-cache-v23';
-const assets = ['/', '/index.html', '/ferie.html', '/malattia.html', '/style.css', '/app.js', '/manifest.json'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
